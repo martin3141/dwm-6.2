97a362d4e2766d44561c36c654724c081f474fa5
Martin Wilson <martin@pipegrep.co.uk>
2019-06-02 07:41:42 +0100

    changed default layout order

--- config.h
+++ config.h
@@ -72,8 +72,8 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -131,8 +131,8 @@ static Key keys[] = {
 	{ MODKEY,                    XK_Tab,      view,           {0} },
 	{ MODKEY|ShiftMask,          XK_q,        killclient,     {0} },
 	{ MODKEY,                    XK_t,        setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                    XK_f,        setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                    XK_m,        setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                    XK_f,        setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                    XK_m,        setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                    XK_space,    setlayout,      {0} },
 	{ MODKEY|ShiftMask,          XK_space,    togglefloating, {0} },
 	{ MODKEY,                    XK_0,        view,           {.ui = ~0 } },
