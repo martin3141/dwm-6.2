cd9bfa992a521058d7a0902b50fe470565f1c082
Martin Wilson <martin@pipegrep.co.uk>
2020-02-18 11:36:21 +0000

    added movestack patch

--- config.h
+++ config.h
@@ -108,6 +108,7 @@ static const char *prevcmd[]     = { "playerctl", "previous", NULL };
 static const char *brightupcmd[] = { "xbacklight", "-inc", "10", NULL };
 static const char *brightdncmd[] = { "xbacklight", "-dec", "10", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                  key          function        argument */
 	{ MODKEY,                    XK_p,        spawn,          {.v = dmenucmd } },
@@ -135,6 +136,8 @@ static Key keys[] = {
 	{ MODKEY,                    XK_d,        incnmaster,     {.i = -1 } },
 	{ MODKEY,                    XK_h,        setmfact,       {.f = -0.05} },
 	{ MODKEY,                    XK_l,        setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,          XK_j,        movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,          XK_k,        movestack,      {.i = -1 } },
 	{ MODKEY,                    XK_Return,   zoom,           {0} },
 	{ MODKEY,                    XK_Tab,      view,           {0} },
 	{ MODKEY|ShiftMask,          XK_q,        killclient,     {0} },
